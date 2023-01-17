9d348e1642a1d8ab7305f54b00ea37f50ae44303
Grace Joh <graceeunaejoh@gmail.com>
2023-01-16 23:38:46 -0500

    arrays

--- arrays.rb
+++ arrays.rb
@@ -1,37 +1,61 @@
 # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 # words = ["guitar", "piano", "flute"]
-
+# words << "drums"
+# words << "cowbell"
+# p words
 
 # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
 # letters = ["a", "r", "w", "p"]
-
+# letters[1] = 34
+# p letters
 
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 # numbers = [1, 2, 3, 4, 5]
+# index = 0
+# while index < numbers.length
+#   p numbers[index]
+#   index += 1
+# end
 
 # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
 # numbers = [3]
-
+# numbers << 1
+# numbers << 2
+# numbers << 3
+# p numbers
 
 # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
 # strings = ["hello", "hi", "hey"]
-
+# strings[2] = strings[2].upcase
+# p strings
 
 # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
 # names = ["grace", "eunice", "maddy"]
-
+# index = 0
+# while index < names.length
+#   p names[index]
+#   index += 1
+# end
 
 # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
 # strings = ["black", "white"]
-
+# strings << "gray"
+# p strings
 
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
 # numbers = [1, 2, 3, 4, 5]
-
+# numbers[0] = numbers[0] * 10
+# p numbers
 
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 # numbers = [12, 24]
-
+# index = 0
+# while index < numbers.length
+#   p numbers[index]
+#   index += 1
+# end
 
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
-countries = ["korea", "usa", "canada"]
+# countries = ["korea", "usa", "canada"]
+# countries << "england"
+# p countries
