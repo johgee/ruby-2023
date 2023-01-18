5800ecc1401d7a8cacd7c90e3cc32d15e22010bd
Grace Joh <graceeunaejoh@gmail.com>
2023-01-17 23:29:43 -0500

    map array

--- functions.rb
+++ functions.rb
@@ -1,28 +1,69 @@
 # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+# def get_double(number)
+#   return number * 2
+# end
 
+# p get_double(2)
 
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+# def upcase(string)
+#   return string.upcase
+# end
 
+# p upcase("hello")
 
 # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+# def get_difference(number1, number2)
+#   return number1 - number2
+# end
 
+# p get_difference(5, 3)
 
 # 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+# def square(number)
+#   return number * number
+# end
 
+# p square(4)
 
 # 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+# def first_letter(string)
+#   return string[0]
+# end
 
+# p first_letter("hello")
 
 # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
+# def combiner(string1, string2, string3)
+#   return "#{string1} #{string2} #{string3}"
+# end
 
+# p combiner("the", "cat", "jumped")
 
 # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+# def convert_to_string(number)
+#   return number.to_s
+# end
 
+# p convert_to_string(5)
 
 # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
+# def repeat(string)
+#   return string * 5
+# end
 
+# p repeat("hello")
 
 # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
+# def return_average(number1, number2, number3)
+#   return (number1 + number2 + number3) / 3.0
+# end
+
+# p return_average(10, 20, 30)
 
 # 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+# def return_times(number)
+#   return number * 10 + 30
+# end
 
+# p return_times(3)
